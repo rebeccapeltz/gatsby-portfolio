b70ca597bffd9714d79fd11bf410954a9be52f13
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-11-18 18:24:16 -0800

    update hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,6 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
 const query = graphql`
   {
     file(relativePath: { eq: "hero-img.png" }) {
@@ -15,31 +14,31 @@ const query = graphql`
     }
   }
 `
-
 const Hero = () => {
-  const data = useStaticQuery(query)
-  console.log(data)
-  const { 
-    file: { 
-      childImageSharp: { fluid } 
-    } 
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
   } = useStaticQuery(query)
-  return <header className="hero">
-    <div className="section-center hero-center">
-      <article className="hero-info">
-      <div>
+
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
             <div className="underline"></div>
-            <h1>Sasha Wolffe</h1>
-            <h4>freelance photography: image and video</h4>
-            <a  className="btn" href="mailto:sasha@cats.com">Contact</a>
-          
+            <h1>i'm john</h1>
+            <h4>freelance web and mobile UI/UX Designer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
             <SocialLinks />
           </div>
-
-      </article>
-      <Image fluid={fluid} className="hero-img"></Image>
-    </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
     </header>
+  )
 }
 
 export default Hero
